c21ddb49d9aa614c9888034de592722da88f1d89
AD\wesleysmith1 <wesleysmith1@weber.edu>
2017-12-22 13:02:16 -0700

    added more content

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ $(document).ready(function(){
     $(document).scroll(function() {
         scroll_start = $(this).scrollTop();
         if(scroll_start > offset.top - 84) {
-            $('#nav').css('background-color', 'black');
+            // $('#feature-1').removeClass("hidden");
+            // $('#feature-2').removeClass("hidden");
+            // $('#feature-3').removeClass("hidden");
+            // $('#feature-4').removeClass("hidden");
+            // $('#feature-5').removeClass("hidden");
+            // $('#feature-6').removeClass("hidden");
+            $('#nav').css('background-color', 'rgba(0, 0, 0, 0.7)');
         } else {
             $('#nav').css('background-color', 'transparent');
         }
